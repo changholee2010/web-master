d8b273bc87e4cd202bd728bcb867ac2c92ea5a13
admin <admin@email.com>
2025-09-11 17:47:47 +0900

    조건문 완료

--- public/js/condition.js
+++ public/js/condition.js
@@ -2,3 +2,25 @@
   condition.js
 */
 console.log("연산자 커밋 이후");
+
+function calculate() {
+  let first = document.querySelector("#user1").value; // 문자열 숫자.
+  let last = document.querySelector("#user2").value; // 문자열 숫자.
+
+  let opr = document.querySelector("#oper").value; // 연산자 구하기.
+  let result = 0; // 결과값을 저장.
+  console.log(first, last, opr);
+  //조건문.
+  switch (opr) {
+    case '+':
+      result = parseInt(first) + parseInt(last); break; // 아래부분으로 진행 break;
+    case '-':
+      result = first - last; break;
+    case '*':
+      result = first * last; break;
+    case '/':
+      result = first / last;
+  }
+  document.querySelector('#result').value = result;
+
+} // end of calculate().
